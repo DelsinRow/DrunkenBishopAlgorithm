0128c4251963d169396137db2c290c0fa5f1e5fc
Evgenii Sinaev <evgenii.sinaev@mera.com>
2023-03-10 10:49:27 +0100

    Added assembly to a jar-file

--- src/main/java/com/orioninc/Main.java
+++ src/main/java/com/orioninc/Main.java
@@ -1,5 +1,7 @@
 package com.orioninc;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,19 +13,23 @@ public class Main {
     private static final String CACHE_ALGORITHM_MD5 = "MD5";
     static int iPos = 4;
     static int jPos = 8;
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
+//        String myString = "{\"key\" : \"Some text\"}";
+//        JSONObject job = new JSONObject(myString);
+        System.out.print("Please, enter the key: ");
         String key = inputSomething();
+//        System.out.println(job.get("key"));
         String fingerprint = fingerprint(keyToMd5(key));
-        System.out.println("You entered: " + key);
+        System.out.println("\nYou entered: " + key);
         printFingerPrint(fingerprint);
 //        System.out.println("Bits array: " + Arrays.deepToString(bitsArray(toBinary(keyToMd5(key)))));
         int[][] array = createField(bitsArray(toBinary(keyToMd5(key))));
-        System.out.println("\nGraphic key:");
         printCaptureArray(graphicKey(array));
 
     }
 
-    static String inputSomething() {
+    private static String inputSomething() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = null;
         try {
@@ -33,20 +39,19 @@ public class Main {
         }
         return input;
     }
+
     private static byte[] keyToMd5(String plainText) throws NoSuchAlgorithmException {
         MessageDigest md5 = MessageDigest.getInstance(CACHE_ALGORITHM_MD5);
         return md5.digest(plainText.getBytes());
     }
 
-    private static String toBinary( byte[] bytes ) {
+    private static String toBinary(byte[] bytes) {
         int count = 0;
-
         StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
-
-        for( int i = 0; i < Byte.SIZE * bytes.length; i++ ) {
+        for (int i = 0; i < Byte.SIZE * bytes.length; i++) {
             sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
             count++;
-            if(count % 2 == 0) sb.append(",");
+            if (count % 2 == 0) sb.append(",");
         }
         sb.setLength(sb.length() - 1);
         return sb.toString();
@@ -56,19 +61,20 @@ public class Main {
         int count = 0;
         int i = 0;
         int j = 0;
-        String [][] binaryArray = new String[16][4];
-        for(String str : binaryCode.split(",")) {
+        String[][] binaryArray = new String[16][4];
+        for (String str : binaryCode.split(",")) {
             binaryArray[i][j] = str;
             j++;
             count++;
-            if(count % 4 == 0) {
+            if (count % 4 == 0) {
                 i++;
                 j = 0;
             }
         }
         return binaryArray;
     }
-    private static String fingerprint (byte[] bytes) {
+
+    private static String fingerprint(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte b : bytes) {
             builder.append(String.format("%02X ", b));
@@ -78,29 +84,29 @@ public class Main {
 
     private static int[][] createField(String[][] bitsArray) {
         int[][] fieldArray = new int[9][17];
-        for(int[] a : fieldArray) {
+        for (int[] a : fieldArray) {
             for (int b : a) {
                 b = 0;
             }
         }
 
         for (String[] fingerByte : bitsArray) {
-           for (int k = fingerByte.length - 1; k >= 0; k--) {
-               switch (fingerByte[k]) {
-                   case "00" -> moveLeftUp(fieldArray);
-                   case "01" -> moveRightUp(fieldArray);
-                   case "10" -> moveLeftDown(fieldArray);
-                   case "11" -> moveRightDown(fieldArray);
-               }
-               fieldArray[iPos][jPos]++;
-           }
+            for (int k = fingerByte.length - 1; k >= 0; k--) {
+                switch (fingerByte[k]) {
+                    case "00" -> moveLeftUp(fieldArray);
+                    case "01" -> moveRightUp(fieldArray);
+                    case "10" -> moveLeftDown(fieldArray);
+                    case "11" -> moveRightDown(fieldArray);
+                }
+                fieldArray[iPos][jPos]++;
+            }
         }
         return fieldArray;
     }
 
-    static void moveLeftUp(int[][] captureArray) { //T == b, L == c, a
+    private static void moveLeftUp(int[][] captureArray) { //T == b, L == c, a
         if (iPos == 0) {
-            if (jPos == 0);                     //a
+            if (jPos == 0) ;                     //a
             else jPos--;                        //T == b
         } else if (jPos == 0) iPos--;           //c == L
         else {
@@ -108,9 +114,10 @@ public class Main {
             jPos--;
         }
     }
-    static void moveRightUp(int[][] captureArray) { //T == a, R == d, b
+
+    private static void moveRightUp(int[][] captureArray) { //T == a, R == d, b
         if (iPos == 0) {
-            if (jPos == 16);                    //b
+            if (jPos == 16) ;                    //b
             else jPos++;                        //a == T
         } else if (jPos == 16) iPos--;          //d == R
         else {
@@ -118,9 +125,10 @@ public class Main {
             jPos++;
         }
     }
-    static void moveLeftDown(int[][] captureArray) { // B==d, L==a,c
+
+    private static void moveLeftDown(int[][] captureArray) { // B==d, L==a,c
         if (iPos == 8) {
-            if (jPos == 0);                     //c
+            if (jPos == 0) ;                     //c
             else jPos--;                        //d == B
         } else if (jPos == 0) iPos++;           //a == L
         else {
@@ -128,33 +136,34 @@ public class Main {
             jPos--;
         }
     }
-    static void moveRightDown(int[][] captureArray) { //B==c, R==b, d
+
+    private static void moveRightDown(int[][] captureArray) { //B==c, R==b, d
         if (iPos == 8) {
-            if (jPos == 16);                    //d
+            if (jPos == 16) ;                    //d
             else jPos++;                        //B == c
-        } else if(jPos == 16) iPos++;           //b == R
+        } else if (jPos == 16) iPos++;           //b == R
     }
 
-    static char[][] graphicKey (int[][] intArray) {
+    private static char[][] graphicKey(int[][] intArray) {
         char[][] graphicKey = new char[9][17];
-        for(int i = 0; i < intArray.length; i++) {
+        for (int i = 0; i < intArray.length; i++) {
             for (int j = 0; j < intArray[i].length; j++) {
                 switch (intArray[i][j]) {
-                    case 0 -> graphicKey[i][j] =  ' ';
-                    case 1 -> graphicKey[i][j] =  '.';
-                    case 2 -> graphicKey[i][j] =  'o';
-                    case 3 -> graphicKey[i][j] =  '+';
-                    case 4 -> graphicKey[i][j] =  '=';
-                    case 5 -> graphicKey[i][j] =  '*';
-                    case 6 -> graphicKey[i][j] =  'B';
-                    case 7 -> graphicKey[i][j] =  'O';
-                    case 8 -> graphicKey[i][j] =  'X';
-                    case 9 -> graphicKey[i][j] =  '@';
-                    case 10 -> graphicKey[i][j] =  '%';
-                    case 11 -> graphicKey[i][j] =  '&';
-                    case 12 -> graphicKey[i][j] =  '#';
-                    case 13 -> graphicKey[i][j] =  '/';
-                    case 14 -> graphicKey[i][j] =  '^';
+                    case 0 -> graphicKey[i][j] = ' ';
+                    case 1 -> graphicKey[i][j] = '.';
+                    case 2 -> graphicKey[i][j] = 'o';
+                    case 3 -> graphicKey[i][j] = '+';
+                    case 4 -> graphicKey[i][j] = '=';
+                    case 5 -> graphicKey[i][j] = '*';
+                    case 6 -> graphicKey[i][j] = 'B';
+                    case 7 -> graphicKey[i][j] = 'O';
+                    case 8 -> graphicKey[i][j] = 'X';
+                    case 9 -> graphicKey[i][j] = '@';
+                    case 10 -> graphicKey[i][j] = '%';
+                    case 11 -> graphicKey[i][j] = '&';
+                    case 12 -> graphicKey[i][j] = '#';
+                    case 13 -> graphicKey[i][j] = '/';
+                    case 14 -> graphicKey[i][j] = '^';
                 }
             }
         }
@@ -164,16 +173,20 @@ public class Main {
         return graphicKey;
     }
 
-    static void printCaptureArray (char[][] array) {
-        for(char[] a : array) {
+    private static void printCaptureArray(char[][] array) {
+        System.out.println("+-----RSA Key-----+");
+        for (char[] a : array) {
+            System.out.print("|");
             for (int i = 0; i < a.length; i++) {
                 System.out.print(a[i]);
             }
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("+-----------------+");
     }
 
-    static void printFingerPrint (String fingerprint) {
+    private static void printFingerPrint(String fingerprint) {
         String fb = fingerprint.replace(' ', ':');
         System.out.println("Fingerprint: " + fb.substring(0, fb.length() - 1));
     }
